0599ca0faeb0ba87b3254c334712e2999ac5215d
nikzan <nikzanvv@gmail.com>
2026-06-23 03:30:44 +0500

    initial commit

---
+++ CoffeeForge/CoffeeForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeForgeApp.swift
+//  CoffeeForge
+//
+//  Created by Nikita Zanadvornykh on 6/23/26.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeForgeTests/CoffeeForgeTests.swift
@@ -0,0 +1,19 @@
+//
+//  CoffeeForgeTests.swift
+//  CoffeeForgeTests
+//
+//  Created by Nikita Zanadvornykh on 6/23/26.
+//
+
+import Testing
+@testable import CoffeeForge
+
+struct CoffeeForgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
